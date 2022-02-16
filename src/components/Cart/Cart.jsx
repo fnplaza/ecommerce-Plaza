dab417150c98238bdbf7c2e6e4311dd302312a53
fnplaza <fnplaza@gmail.com>
2022-02-16 09:51:11 -0300

    Contador added!

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { useState } from "react";
 
 const Cart = () => {
-  return <div>Cart</div>;
+  const [contador, setContador] = useState(0);
+  return (
+    <div>
+      <h1>{contador}</h1>
+      <button onClick={() => setContador(contador + 1)}>+</button>
+      <button onClick={() => setContador(contador - 1)}>-</button>
+    </div>
+  );
 };
 
 export default Cart;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -6,11 +6,13 @@ import NavBar from "../components/Navbar/NavBar";
 import Footer from "../components/UI/Footer";
 import Button from "../components/UI/Button";
 import Accordion from "../components/Accordion/Accordion";
+import Cart from "../components/Cart/Cart";
 
 const Rutas = () => {
   return (
     <BrowserRouter>
       <NavBar textoDeNav={"HopeIndumentaria"} />
+      <Cart />
       <Routes>
         <Route path="/" element={<ItemListContainer />} />
         <Route path="/checkout" element={<Checkout />} />
